01a8542111ad78bb05873aabe33f6365f802b51d
Cristian <60708997+cmioveanu@users.noreply.github.com>
2020-12-08 14:21:16 +0000

    add onclick to main title

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import styles from './Header.module.css';
 
+import {useDispatch} from 'react-redux';
+import {changeActiveSubreddit } from '../subreddits/subredditsSlice';
+
 import {SearchBar} from './searchBar/SearchBar';
 
 export const Header = () => {
+    const dispatch = useDispatch();
+
+    const onTitleClicked = () => {
+        dispatch(changeActiveSubreddit("/r/Home/"));
+    }
+
     return (
         <header className={styles.mainHeader}>
-            <h1>Reddit<span>Minimal</span></h1>
+            <h1 onClick={onTitleClicked}>Reddit<span>Minimal</span></h1>
             <SearchBar className={styles.searchBar}/>
         </header>
     );

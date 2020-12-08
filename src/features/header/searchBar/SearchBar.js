e8af225df41a3d5edb30ac1283e656c18c49be37
Cristian <60708997+cmioveanu@users.noreply.github.com>
2020-12-08 14:12:36 +0000

    add subreddit to search bar

--- src/features/header/searchBar/SearchBar.js
+++ src/features/header/searchBar/SearchBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useState} from 'react';
+import {useSelector} from 'react-redux';
 
 import styles from './SearchBar.module.css';
 
 export const SearchBar = () => {
     const [search, setSearch] = useState("");
-
+    const activeSubSliced = useSelector(state => state.subreddits.activeSubreddit).slice(3, -1);
+    console.log(activeSubSliced);
     const onTextChange = (e) => {
         setSearch(e.target.value);
     }
@@ -14,7 +16,7 @@ export const SearchBar = () => {
         <input className={styles.searchBar}
             id="Search"
             value={search}
-            placeholder="Search posts"
+            placeholder={"Search posts in " + activeSubSliced}
             onChange={onTextChange}
         />
     );
